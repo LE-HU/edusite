155046cfdc6315e60f77b2c0be3ad35d75200549
LE-HU <wojciechlech1337@gmail.com>
2020-10-07 13:09:06 +0200

    styling courses and home index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
+  skip_before_action :authenticate_user!, :only => [:index]
+
   def index
+    @courses = Course.all.limit(3)
+    @latest_courses = Course.all.order(created_at: :desc).limit(3)
   end
 end
