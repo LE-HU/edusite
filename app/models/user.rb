4ff25fe05dfed22b17a2907cc886d49e9bd252ef
LE-HU <wojciechlech1337@gmail.com>
2020-10-08 19:46:43 +0200

    install rolify & pundit gems, add roles for users using rolify

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,24 @@
 class User < ApplicationRecord
+  rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
 
   has_many :courses
+
+  after_create :assign_default_role
+
+  def assign_default_role
+    if User.count == 1
+      self.add_role(:admin) if self.roles.blank?
+      self.add_role(:teacher)
+      self.add_role(:student)
+    else
+      self.add_role(:student) if self.roles.blank?
+      self.add_role(:teacher)
+    end
+  end
 
   def to_s
     email
