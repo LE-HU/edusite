6bc212a4883fabe8491aafeb414c0da972d27440
LE-HU <wojciechlech1337@gmail.com>
2020-10-10 20:46:45 +0200

    add user edit action, edit view, feature to update user roles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,32 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all.order(created_at: :desc)
+  before_action :set_user, only: [:show, :edit, :update]
 
+  def index
     @q = User.ransack(params[:q])
     @users = @q.result(distinct: true)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to users_path, notice: "User #{@user.email} was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit({ role_ids: [] })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :courses
-  resources :users, only: [:index]
+  resources :users, only: [:index, :show, :edit, :update]
   root "home#index"
   get "home/activity"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
