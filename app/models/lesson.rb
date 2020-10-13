1e6adfcfd2d2001fa170e43345691bf1aa501515
LE-HU <wojciechlech1337@gmail.com>
2020-10-13 14:49:53 +0200

    fix double notice upon lesson creation, add breadcrumbs to lesson show, add rich text content form input for lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,6 +3,7 @@ class Lesson < ApplicationRecord
   friendly_id :title, use: :slugged
   include PublicActivity::Model
   tracked owner: Proc.new { |controller, model| controller.current_user }
+  has_rich_text :content
 
   validates :title, :content, :course, presence: true
   belongs_to :course
