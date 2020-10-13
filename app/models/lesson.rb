26c5eae2920cc9fb6354240f02e7c7633989bb76
LE-HU <wojciechlech1337@gmail.com>
2020-10-13 14:44:10 +0200

    add activity tracker for lessons, enable lesson creation inside course show page

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,7 +1,13 @@
 class Lesson < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
+  include PublicActivity::Model
+  tracked owner: Proc.new { |controller, model| controller.current_user }
 
   validates :title, :content, :course, presence: true
   belongs_to :course
+
+  def to_s
+    title
+  end
 end
