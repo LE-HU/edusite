de2d2b763ad4e9d0710cb8611eeb7b790de4d21c
LE-HU <wojciechlech1337@gmail.com>
2020-11-09 01:27:06 +0100

    add friendly_id to enrollments

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,4 +1,7 @@
 class Enrollment < ApplicationRecord
+  extend FriendlyId
+  friendly_id :to_s, use: :slugged
+
   belongs_to :course
   belongs_to :user
 
@@ -8,6 +11,10 @@ class Enrollment < ApplicationRecord
   validate :cant_subscribe_to_own_course
 
   scope :pending_review, -> { where(rating: [0, nil, ""], review: [0, nil, ""]) }
+
+  def to_s
+    user.to_s + " " + course.to_s
+  end
 
   protected
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_135637) do
+ActiveRecord::Schema.define(version: 2020_11_09_002227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,7 +88,9 @@ ActiveRecord::Schema.define(version: 2020_10_14_135637) do
     t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
     t.index ["course_id"], name: "index_enrollments_on_course_id"
+    t.index ["slug"], name: "index_enrollments_on_slug", unique: true
     t.index ["user_id"], name: "index_enrollments_on_user_id"
   end
 
