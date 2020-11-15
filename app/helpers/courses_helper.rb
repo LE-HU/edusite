f366bb9359f2cf0c98e0ebe26bf518628cccb756
LE-HU <wojciechlech1337@gmail.com>
2020-11-15 19:21:30 +0100

    add ransack filtering to courses index page

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -2,7 +2,7 @@ module CoursesHelper
   def enrollment_button(course)
     if current_user
       if course.user == current_user
-        link_to "View analytics", course_path(course), class: "btn btn-sm btn-info"
+        link_to "View analytics", course_path(course), class: "btn btn-sm btn-secondary"
       elsif course.enrollments.where(user: current_user).any?
         link_to "Keep learning", course_path(course), class: "btn btn-sm btn-primary"
       elsif course.price > 0
@@ -11,7 +11,7 @@ module CoursesHelper
         link_to "Free", new_course_enrollment_path(course), class: "btn btn-sm btn-success"
       end
     else
-      link_to "Check price", course_path(course), class: "btn btn-sm btn-info"
+      link_to "Check price", course_path(course), class: "btn btn-sm btn-primary"
     end
   end
 
@@ -20,7 +20,7 @@ module CoursesHelper
       user_course = course.enrollments.where(user: current_user)
       if user_course.any?
         if user_course.pending_review.any?
-          link_to "Add a review", edit_enrollment_path(user_course.first), class: "btn btn-sm btn-info my-2"
+          link_to "Add a review", edit_enrollment_path(user_course.first), class: "btn btn-sm btn-primary my-2"
         else
           link_to "See your review", enrollment_path(user_course.first), class: "btn btn-sm btn-secondary my-2"
         end
