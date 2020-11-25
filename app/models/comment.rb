978b1de9926dac4b78aeac1b838df13c4ef4e1de
LE-HU <wojciechlech1337@gmail.com>
2020-11-25 16:30:43 +0100

    add PublicActivity for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
+  include PublicActivity::Model
+  tracked owner: Proc.new { |controller, model| controller.current_user }
+
   belongs_to :user, counter_cache: true
   belongs_to :lesson, counter_cache: true
   validates :content, presence: true
