1a995741deb76a12e504074d632dbbbf95f1657b
LE-HU <wojciechlech1337@gmail.com>
2020-11-25 17:17:10 +0100

    add counter_cache to user_lessons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_014411) do
+ActiveRecord::Schema.define(version: 2020_11_25_161241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -129,6 +129,7 @@ ActiveRecord::Schema.define(version: 2020_11_25_014411) do
     t.string "slug"
     t.integer "row_order"
     t.integer "comments_count", default: 0, null: false
+    t.integer "user_lessons_count", default: 0, null: false
     t.index ["course_id"], name: "index_lessons_on_course_id"
     t.index ["slug"], name: "index_lessons_on_slug", unique: true
   end
@@ -168,6 +169,7 @@ ActiveRecord::Schema.define(version: 2020_11_25_014411) do
     t.integer "courses_count", default: 0, null: false
     t.integer "enrollments_count", default: 0, null: false
     t.integer "comments_count", default: 0, null: false
+    t.integer "user_lessons_count", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true
